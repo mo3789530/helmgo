e14904f4b29d3a49c36bda66e9cde81a1ba4d5f4
mo3789530 <mo3789530@gmail.com>
2022-02-12 15:20:25 +0900

    add k8s client

--- go.mod
+++ go.mod
@@ -129,8 +129,8 @@ require (
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	helm.sh/helm v2.17.0+incompatible
 	helm.sh/helm/v3 v3.8.0
-	k8s.io/api v0.23.3 // indirect
-	k8s.io/apimachinery v0.23.3 // indirect
+	k8s.io/api v0.23.3
+	k8s.io/apimachinery v0.23.3
 	k8s.io/klog/v2 v2.30.0 // indirect
 	k8s.io/kube-openapi v0.0.0-20211115234752-e816edb12b65 // indirect
 	k8s.io/utils v0.0.0-20211116205334-6203023598ed // indirect
--- src/client.go
+++ src/client.go
@@ -1,11 +1,17 @@
 package src
 
-import "k8s.io/client-go/kubernetes"
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
 
 type K8SClient interface {
 	GetNameSpaces() ([]string, error)
-	// GetPods(namespace string) ([]string, error)
-	// CreateNameSpace(namespace string) error
+	GetPods(ns string) ([]string, error)
+	CreateNameSpace(Name string) error
 }
 
 type k8sClient struct {
@@ -19,5 +25,49 @@ func Newk8sClient(clientset *kubernetes.Clientset) K8SClient {
 }
 
 func (c *k8sClient) GetNameSpaces() ([]string, error) {
-	return nil, nil
+	ns, err := c.Clientset.CoreV1().Namespaces().List(
+		context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	var arr []string
+	for _, v := range ns.Items {
+		arr = append(arr, v.ObjectMeta.Name)
+	}
+	return arr, err
+}
+
+func (c *k8sClient) GetPods(ns string) ([]string, error) {
+	pods, err := c.Clientset.CoreV1().Pods(ns).List(
+		context.TODO(), metav1.ListOptions{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var arr []string
+	for _, v := range pods.Items {
+		arr = append(arr, v.ObjectMeta.Name)
+	}
+	return arr, nil
+}
+
+func (c *k8sClient) CreateNameSpace(name string) error {
+	object := &corev1.Namespace{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Namespace",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Spec: corev1.NamespaceSpec{},
+	}
+
+	_, err := c.Clientset.CoreV1().Namespaces().Create(
+		context.TODO(), object, metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+	return nil
 }
